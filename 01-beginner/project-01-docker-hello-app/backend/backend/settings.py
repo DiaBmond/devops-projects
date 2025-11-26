a15f0f6d80d626e1ec0cc58f0e3dba7a85c4d3de
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2025-11-26 13:09:26 +0700

    feat: add frontend add project

--- 01-beginner/project-01-docker-hello-app/backend/backend/settings.py
+++ 01-beginner/project-01-docker-hello-app/backend/backend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,6 +81,10 @@ DATABASES = {
     }
 }
 
+# allow only http://localhost:3000
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
